389e30f6abe1558e7fce92e91a8ae8aaf7010f89
max-steele <msteele1100@gmail.com>
2025-06-21 17:59:00 -0700

    add dark logo

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,17 +1,24 @@
+"use client";
+
 import Image, { ImageProps } from "next/image";
 import { cx } from "../utils/joinClassNames";
+import { useTheme } from "../context/ThemeContext";
 
 export const Logo: React.FC<Partial<ImageProps>> = ({
   className,
   ...props
-}) => (
-  <Image
-    className={cx(className)}
-    src={"/ms-logo.svg"}
-    alt={`ms logo`}
-    width={16}
-    height={16}
-    priority
-    {...props}
-  />
-);
+}) => {
+  const { theme } = useTheme();
+  
+  return (
+    <Image
+      className={cx(className)}
+      src={theme === "dark" ? "/ms-logo-dark.svg" : "/ms-logo.svg"}
+      alt={`ms logo`}
+      width={16}
+      height={16}
+      priority
+      {...props}
+    />
+  );
+};
